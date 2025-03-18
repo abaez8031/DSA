55f5beeabda3508439ef8fa3e851ba74dabb6059
abaez8031 <abaez8031@gmail.com>
2025-03-18 17:08:45 -0400

    maxsubarraysum

--- same.js
+++ same.js
@@ -180,13 +180,36 @@ function findPair(array, n) {
   return false;
 }
 
-console.log(findPair([6,1,4,10,2,4], 2)) // true
-console.log(findPair([8,6,2,4,1,0,2,5,13],1)) // true
-console.log(findPair([4,-2,3,10],-6)) // true
-console.log(findPair([6,1,4,10,2,4], 22)) // false
-console.log(findPair([], 0)) // false
-console.log(findPair([5,5], 0)) // true
-console.log(findPair([-4,4], -8)) // true
-console.log(findPair([-4,4], 8)) // true
-console.log(findPair([1,3,4,6],-2)) // true
-console.log(findPair([0,1,3,4,6],-2)) // true
+// console.log(findPair([6,1,4,10,2,4], 2)) // true
+// console.log(findPair([8,6,2,4,1,0,2,5,13],1)) // true
+// console.log(findPair([4,-2,3,10],-6)) // true
+// console.log(findPair([6,1,4,10,2,4], 22)) // false
+// console.log(findPair([], 0)) // false
+// console.log(findPair([5,5], 0)) // true
+// console.log(findPair([-4,4], -8)) // true
+// console.log(findPair([-4,4], 8)) // true
+// console.log(findPair([1,3,4,6],-2)) // true
+// console.log(findPair([0,1,3,4,6],-2)) // true
+
+function maxSubarraySum(array, n){
+  // add whatever parameters you deem necessary - good luck!
+  if(array.length < n) return null;
+  let maxSum = -Infinity;
+  let tempSum = 0;
+  for(let i = 0; i < n; i++) {
+    tempSum += array[i];
+  }
+  maxSum = Math.max(tempSum, maxSum);
+  for(let i = n; i < array.length; i++) {
+    tempSum = tempSum + array[i] - array[i - n];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+// console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+// console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+// console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+// console.log(maxSubarraySum([2,3], 3)) // null
+
