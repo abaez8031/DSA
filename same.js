017e0a6ae19ed94d2e48a2684496dba4eae28b80
abaez8031 <abaez8031@gmail.com>
2025-03-23 23:55:29 -0400

    findpairtwopointer

--- same.js
+++ same.js
@@ -196,16 +196,16 @@ function findPair(array, n) {
   return false;
 }
 
-// console.log(findPair([6,1,4,10,2,4], 2)) // true
-// console.log(findPair([8,6,2,4,1,0,2,5,13],1)) // true
-// console.log(findPair([4,-2,3,10],-6)) // true
-// console.log(findPair([6,1,4,10,2,4], 22)) // false
-// console.log(findPair([], 0)) // false
-// console.log(findPair([5,5], 0)) // true
-// console.log(findPair([-4,4], -8)) // true
-// console.log(findPair([-4,4], 8)) // true
-// console.log(findPair([1,3,4,6],-2)) // true
-// console.log(findPair([0,1,3,4,6],-2)) // true
+console.log(findPair([6,1,4,10,2,4], 2)) // true
+console.log(findPair([8,6,2,4,1,0,2,5,13],1)) // true
+console.log(findPair([4,-2,3,10],-6)) // true
+console.log(findPair([6,1,4,10,2,4], 22)) // false
+console.log(findPair([], 0)) // false
+console.log(findPair([5,5], 0)) // true
+console.log(findPair([-4,4], -8)) // true
+console.log(findPair([-4,4], 8)) // true
+console.log(findPair([1,3,4,6],-2)) // true
+console.log(findPair([0,1,3,4,6],-2)) // true
 
 function maxSubarraySum(array, n){
   // add whatever parameters you deem necessary - good luck!
